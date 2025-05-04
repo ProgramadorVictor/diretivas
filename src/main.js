6cfa02a89ebba4e3914df30720cec336e411459f
ProgramadorVictor <victorochaandrade@gmail.com>
2025-05-04 20:36:22 -0300

    criando uma diretiva personalizada

--- src/main.js
+++ src/main.js
@@ -1,4 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// createApp(App).mount('#app')
+
+
+/* Criação de diretivas personalizadas */
+const Vue = createApp(App)
+
+//Diretiva personalizada chamada teste.
+Vue.directive('teste', {
+    created(){ //Chamamos um hook do ciclo de vida antes que os atributos do elemento ou ouvintes de eventos (event listeners) sejam aplicados.
+        console.log("A diretiva foi aplicada com sucesso!");
+    }
+})
+
+Vue.mount('#app') //Montando a aplicação
