7fd85be6c3a3f4f50221ef09295a074e03d7db62
ProgramadorVictor <victorochaandrade@gmail.com>
2025-05-09 21:29:59 -0300

    modificando a exibição com base no argumento

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
   <!-- Abaixo estamos fazendo o binding de dois valores diferentes na diretiva. Do argumento 'absolute' e do valor '75'. -->
   <div v-posicao:absolute="75">Olá, eu sou um binding de argumento e valores</div>
   <span>Código A: <input type="text"><span v-informacao:simples.umClickMouse="'Você encontrará o código A na parte frontal do equipamento.'">i</span></span><br>
-  <span>Código B: <input type="text"><span v-informacao:simples.doisClickMouse="'Você encontrará o código B na parte trazeira do equipamento.'">i</span></span>
+  <span>Código B: <input type="text"><span v-informacao:destacado.doisClickMouse="'Você encontrará o código B na parte trazeira do equipamento.'">i</span></span>
 </template>
 
 <script>
@@ -14,4 +14,7 @@ export default {
     configuracaoInicial: {cor: 'red'}
   })
 }
+
+// v-informacao:destacado.doisClickMouse
+// diretiva:argumento.modificador
 </script>
--- src/main.js
+++ src/main.js
@@ -57,22 +57,56 @@ Vue.directive('posicao', {
 Vue.directive('informacao', {
     created(el, binding){
         console.log(el, binding, binding.modifiers, binding.value);
-        
-        let funcao = function(event){
-            console.log(event)
+        let funcao = null
 
-            let informacaoSpan = document.createElement('span');
-            informacaoSpan.style.position = 'absolute'
-            informacaoSpan.style.background = '#ddd'
-            informacaoSpan.style.padding = '2px'
-            informacaoSpan.innerText = binding.value
+        if(binding.arg == undefined || binding.arg === 'simples'){
+            funcao = function(event){
+                console.log(event)
+    
+                let informacaoSpan = document.createElement('span');
+                informacaoSpan.style.position = 'absolute'
+                informacaoSpan.style.background = '#ddd'
+                informacaoSpan.style.padding = '2px'
+                informacaoSpan.innerText = binding.value
+    
+                el.appendChild(informacaoSpan);
+    
+                informacaoSpan.addEventListener('click', (event) => {
+                    event.stopPropagation() //Para não ficar gerando vários span
+                    informacaoSpan.remove()
+                })
+            }
+        }
 
-            el.appendChild(informacaoSpan);
+        if(binding.arg === 'destacado'){
 
-            informacaoSpan.addEventListener('click', (event) => {
-                event.stopPropagation() //Para não ficar gerando vários span
-                informacaoSpan.remove()
-            })
+            funcao = function(){
+                let informacaoDivContainer = document.createElement('div')
+                let informacaoDiv = document.createElement('div')
+                informacaoDiv.innerText = binding.value
+
+                informacaoDivContainer.style.width = '100%'
+                informacaoDivContainer.style.height = '100%'
+                informacaoDivContainer.style.display = 'flex'
+                informacaoDivContainer.style.flexDirection = 'row'
+                informacaoDivContainer.style.alignItems = 'center'
+                informacaoDivContainer.style.justifyContent = 'center'
+                informacaoDivContainer.style.background = '#ccc'
+                informacaoDivContainer.style.position = 'absolute'
+                informacaoDivContainer.style.top = '0'
+                informacaoDivContainer.style.zIndex = '10'
+
+                informacaoDiv.style.padding = '30px'
+                informacaoDiv.style.background = '#fba'
+                informacaoDivContainer.appendChild(informacaoDiv)
+
+                el.appendChild(informacaoDivContainer)
+
+                informacaoDivContainer.addEventListener('click', (event) => {
+                    event.stopPropagation() //Para não ficar gerando vários span
+                    informacaoDivContainer.remove()
+                })
+            }
         }
 
         if(binding.modifiers['umClickMouse']){
